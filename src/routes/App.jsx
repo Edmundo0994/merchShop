c56acd86f0bff87ff8205136c078b87a691800bc
Edmundo0994 <esala.094@gmail.com>
2022-03-10 18:26:16 -0500

    [ADD] Created App Context and custom hook to handle initial state

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,21 +1,26 @@
 import React from "react";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import { Home, Checkout, Information, Payment, Success, NotFound } from "../containers";
-import Layout from "../components/layout";
+import Layout from "../components/Layout";
+import AppContext from "../context/AppContext";
+import useInitialState from "../hooks/useInitialState";
 
 export default function App() {
+  const initialState = useInitialState();
   return (
-    <BrowserRouter>
-      <Layout>
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route exact path="/checkout" element={<Checkout />} />
-          <Route exact path="/checkout/information" element={<Information />} />
-          <Route exact path="/checkout/payment" element={<Payment />} />
-          <Route exact path="/checkout/success" element={<Success />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </Layout>
-    </BrowserRouter>
+    <AppContext.Provider value={initialState}>
+      <BrowserRouter>
+        <Layout>
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/checkout" element={<Checkout />} />
+            <Route exact path="/checkout/information" element={<Information />} />
+            <Route exact path="/checkout/payment" element={<Payment />} />
+            <Route exact path="/checkout/success" element={<Success />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </Layout>
+      </BrowserRouter>
+    </AppContext.Provider>
   );
 }
