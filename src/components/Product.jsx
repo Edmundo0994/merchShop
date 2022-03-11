ddb40d17a81e5e0a0f8e41897ebfcdf2fbb59aac
Edmundo0994 <esala.094@gmail.com>
2022-03-10 19:24:35 -0500

    [ADD] Logic to checkout and litle changes in css

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -4,10 +4,10 @@ export default function Product({ product, handleAddToCart }) {
   return (
     <div className="Products-item">
       <img src={product.image} alt={product.title} />
-      <div className="Product-item-info">
+      <div className="Products-item-info">
         <h2>
           {product.title}
-          <span>{product.price}</span>
+          <span>${product.price}</span>
         </h2>
         <p>{product.description}</p>
       </div>
--- src/containers/Checkout.jsx
+++ src/containers/Checkout.jsx
@@ -1,29 +1,49 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
+import AppContext from "../context/AppContext";
 import "../styles/components/Checkout.css";
 
 export default function Checkout() {
+  const { state, removeFromCart } = useContext(AppContext);
+  const { cart } = state;
+  const totalPrice = cart.reduce((a, b) => a + (b.price || 0), 0);
+  const handleRemoveFromCart = (product) => {
+    removeFromCart(product);
+  };
   return (
     <div className="Checkout">
       <div className="Checkout-content">
         <h3>Lista de Pedidos:</h3>
-        <div className="Checkout-item">
-          <div className="Checkout-element">
-            <h4>ITEM name</h4>
-            <span>$10</span>
-          </div>
-          <button type="button">
-            <DeleteOutlineIcon size="small" />
-          </button>
+        {cart.length > 0 ? (
+          cart.map((item) => (
+            <div key={item.id} className="Checkout-item">
+              <div className="Checkout-element">
+                <div className="Checkout-detail">
+                  <img src={item.image} alt={item.title} />
+                  <h4>{item.title}</h4>
+                </div>
+                <span>${item.price}</span>
+              </div>
+              <button type="button" onClick={() => handleRemoveFromCart(item)}>
+                <DeleteOutlineIcon size="small" />
+              </button>
+            </div>
+          ))
+        ) : (
+          <h2>No hay nada dentro del carrito</h2>
+        )}
+      </div>
+      {cart.length > 0 && (
+        <div className="Checkout-sidebar">
+          <h3>Precio Total: ${totalPrice}</h3>
+          <Link to="/checkout/information">
+            <button id="ctn-btn" type="button">
+              Continuar pedido
+            </button>
+          </Link>
         </div>
-      </div>
-      <div className="Checkout-sidebar">
-        <h3>Precio Total: $10</h3>
-        <Link to="/checkout/information">
-          <button type="button">Continuar pedido</button>
-        </Link>
-      </div>
+      )}
     </div>
   );
 }
